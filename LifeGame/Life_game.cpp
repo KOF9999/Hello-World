6575913c3363f94c52836cf8ec418b92110c3a4d
Zhang WB <Zhangweibink@126.com>
2016-09-06 23:00:08 +0800

    change CPP

--- LifeGame/Life_game.cpp
+++ LifeGame/Life_game.cpp
@@ -2,7 +2,22 @@
 using namespace std;
 #define maxLine 20
 #define maxRow 20
+int Cube[maxLine][maxRow];
+int line,row;
+int numL,numR;
 int main()
 {
+    cin>>line>>row;
+    if(line>maxLine||row>maxRow)
+       {
+           cout<<"log:too big"<<endl;
+           return false;
+       }
+    for(numL=0;numL<line;numL++)
+        for(numR=0;numR<row;numR++)
+        {
+            cin>>Cube[numL][numR];
+        }
+
     return 0;
 }
